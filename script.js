e81df4950c26dcb82a45564dba446176c2ab431a
gekishou <gonfuentes112@gmail.com>
2024-10-23 01:50:32 -0300

    feat: ui done

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ const divide = function(a, b) {
 }
 
 const opMap = new Map([['+', add], ['-', subtract], ['*', multiply], ['/', divide]]);
+let operatorA, operatorB;
+
+function operate(operatorA, operatorB, operationButton) {
+    const operation = opMap.get(operationButton);
+    return operation(operatorA, operatorB);
+}
+const DEFAULT_DISPLAY = '0123456789';
+const topDisplay = document.getElementById('topdisplay');
+const bottomDisplay = document.getElementById('bottomdisplay');
+
+function clear(){
+    topDisplay.innerHTML = ""
+    bottomDisplay.innerText = DEFAULT_DISPLAY;
+    display.style.color = "rgb(150, 147, 147)";
+
+}
+
+clear();
